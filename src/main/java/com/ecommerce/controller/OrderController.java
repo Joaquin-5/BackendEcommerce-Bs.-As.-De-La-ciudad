09bad531244d7185a456d2ce6c82001f2b6cca4b
Joaquín Butera <joaquin.butera@gmail.com>
2025-07-08 13:53:58 -0300

    Get simple en el ProductController y en el OrderController

--- src/main/java/com/ecommerce/controller/OrderController.java
+++ src/main/java/com/ecommerce/controller/OrderController.java
@@ -1,4 +1,11 @@
 package com.ecommerce.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+    @GetMapping("/")
+    public String test() {return "Se intentó acceder a los pedidos...";}
 }
